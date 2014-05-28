915247f73a0c0fa2bddb2b94f693c1c813ddcfc9
Erispoe <favrebulle@gmail.com>
2014-05-28 16:44:27 +0200

    Create a table of CBSA values by county, 2010

---
+++ methods/A_CBSA_2010/50_A_CBSA_ByCounties_2010.R
@@ -0,0 +1,29 @@
+# Merge data on CBSA with data on counties
+A_CBSA_ByCounties_2010 <- function() {
+  # Connect to database
+  db <- conma()
+  
+  # Import data
+  counties  <- dbReadTable(db, "C_SF1_Counties_2010")
+  delin     <- dbReadTable(db, "C_MetroDelineations_201302")
+  cbsa      <- dbReadTable(db, "A_CBSA_2010")
+  dbDisconnect(con)
+  
+  counties <- counties[counties$state != "15" & counties$state != "72", ]
+  
+  # Merge  
+  co <- merge(
+    counties,
+    delin[,c("CBSACode", "FIPSStateCode", "FIPSCountyCode")],
+    by.x = c("fipsstate", "fipscounty"),
+    by.y = c("FIPSStateCode","FIPSCountyCode"),
+    all.x = TRUE
+  )
+  
+  co <- merge(co, cbsa, by = "CBSACode", all.x = T)
+  
+  # Write table
+  db <- conma()
+  dbWriteTable(db, name="A_CBSA_ByCounty_2010", value=co, overwrite=TRUE)
+  dbDisconnect(db)
+}
