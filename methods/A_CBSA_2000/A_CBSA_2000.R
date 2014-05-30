596b9a2a50c6d0de2cc4d635167222091146c1fe
Erispoe <favrebulle@gmail.com>
2014-05-30 15:32:00 +0200

    Structure for CBSA 2000

---
+++ methods/A_CBSA_2000/A_CBSA_2000.R
@@ -0,0 +1,17 @@
+# Compute the CBSA table from census data
+# Core Based Statistical Area
+# Create table of CBSA values by county (data used to create maps)
+# Export relevant csv files
+
+A_CBSA_2000 <- function() {
+  A_CBSA_HHI_2000()
+  #A_CBSA_HHI_Counties_2000()
+  #A_CBSA_HHI_Places_2000()
+  #A_CBSA_CountPlaces_2000()
+  #A_CBSA_AddMDPI_2007()
+  #A_CBSA_PopCentral_2000()
+  #A_CBSA_ZieglerBrunn_2000()
+  #A_CBSA_ByCounties_2000()
+  #A_CBSA_2000_Export()
+  #A_CBSA_ByCounty_2000_Export()
+}
