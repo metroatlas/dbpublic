6196506c1a7464da38286267a7f58efa958edfd5
Erispoe <favrebulle@gmail.com>
2014-06-18 14:18:44 +0200

    HHI Index for SMSA 1980

--- methods/A_CBSA_1980/01_A_SMSA_HHI_1980.R
+++ methods/A_CBSA_1980/01_A_SMSA_HHI_1980.R
@@ -12,6 +12,7 @@ A_SMSA_HHI_2010 <- function() {
   placelist <- dbReadTable(db, "C_PlaceList_1980")
   pcodes    <- dbReadTable(db, "C_PlacesVintageCodes")
   smsa      <- dbReadTable(db, "C_MetroNames_1981")
+  delin     <- dbReadTable(db, "C_MetroDelineations_1981")
   dbDisconnect(db)
   
   # Get rid of Hawaii and Puerto Rico
@@ -53,60 +54,62 @@ A_SMSA_HHI_2010 <- function() {
   
   # Clean the columns
   pl <- pl[, c("fips","statefp","placefp","countyfp", "STATE", "countyname","placename.x","totalpop1980")]
+  colnames(pl)[4] <- "countyfips"
   colnames(pl)[5] <- "statename"
   colnames(pl)[7] <- "placename"
   
-  # Counties with SMSA
-  ctytomerge <- counties[,c("fips", "SMSAA")]
-  colnames(ctytomerge) <- c("countyfp", "SMSA")
+  # Delineations of SMSA
+  delin$countyfips <- mapply(paste0, delin$statefp, delin$countyfp, MoreArgs = list(collapse = ""))
+  pl <- merge(pl, delin, by = "countyfips")
+  pl <- pl[, c("fips","statefp.x","placefp","countyfp", "statename", "countyname.x","placename","totalpop1980", "smsa")]
+  colnames(pl)[2] <- "statefp"
+  colnames(pl)[6] <- "countyname"
   
-  pl <- merge(pl, ctytomerge, by = "countyfp")
-  rm(ctytomerge)
-    
   # Counties with SMSA and population
-  co <- counties[,c("fipsstate", "fipscounty", "fips", "STATE", "COUNTY", "totalpop1980", "SMSAA")]
-  colnames(co) <- c("fipsstate", "fipscounty", "fips", "statename", "countyname", "totalpop1980", "SMSA")
+  co <- counties[,c("fipsstate", "fipscounty", "fips", "STATE", "COUNTY", "totalpop1980")]
+  colnames(co) <- c("fipsstate", "fipscounty", "fips", "statename", "countyname", "totalpop1980")
+  co <- merge(co, delin[,c(1,7)], by.x = "fips", by.y = "countyfips")
   
   # Aggregated population by SMSA
-  plco <- rbind(pl[,c("SMSA", "totalpop1980")], co[,c("SMSA", "totalpop1980")])
-  smsa.pop.aggr <- aggregate(plco$totalpop1980, by=list(plco$SMSA), FUN=sum, na.rm=FALSE)
-  names(smsa.pop.aggr)  <- c("SMSA","SMSApop.aggr")
-  smsa.pop.aggr <- smsa.pop.aggr[smsa.pop.aggr$SMSA != "", ]
+  plco <- rbind(pl[,c("smsa", "totalpop1980")], co[,c("smsa", "totalpop1980")])
+  smsa.pop.aggr <- aggregate(plco$totalpop1980, by=list(plco$smsa), FUN=sum, na.rm=FALSE)
+  names(smsa.pop.aggr)  <- c("smsa","SMSApop.aggr")
+  smsa.pop.aggr <- smsa.pop.aggr[smsa.pop.aggr$smsa != "", ]
   
   # Consolidation share of counties
   co <- merge(co, smsa.pop.aggr,
-              by="SMSA",
+              by="smsa",
               all = FALSE)
   
   co$HHI.smsa <- (co$totalpop1980 / co$SMSApop.aggr)^2
   
   #Consolidation share of places
   pl <- merge(pl, smsa.pop.aggr,
-              by="SMSA",
+              by="smsa",
               all = FALSE)
   
   pl$HHI.smsa <- (pl$totalpop1980 / pl$SMSApop.aggr)^2
   
   # Sums of squared shares
-  plco <- rbind(pl[,c("SMSA", "HHI.smsa")], co[,c("SMSA", "HHI.smsa")])
+  plco <- rbind(pl[,c("smsa", "HHI.smsa")], co[,c("smsa", "HHI.smsa")])
   
   # Consolidation index HHI by SMSA
-  smsa.hhi <- aggregate(plco$HHI.smsa, by=list(plco$SMSA), FUN=sum, na.rm=FALSE)
-  names(smsa.hhi)  <- c("SMSA","SMSAhhi")
+  smsa.hhi <- aggregate(plco$HHI.smsa, by=list(plco$smsa), FUN=sum, na.rm=FALSE)
+  names(smsa.hhi)  <- c("smsa","SMSAhhi")
   
   # Total population of CBSA
-  smsa.pop <- aggregate(co$totalpop1980, by=list(co$SMSA), FUN=sum, na.rm=FALSE)
-  names(smsa.pop)  <- c("SMSA","SMSApop")
+  smsa.pop <- aggregate(co$totalpop1980, by=list(co$smsa), FUN=sum, na.rm=FALSE)
+  names(smsa.pop)  <- c("smsa","SMSApop")
   
-  smsa.temp <- merge(smsa.pop, smsa.hhi, by = "SMSA")
-  colnames(smsa) <- c("SMSA", "SCSA", "SMSAname")
-  smsa <- merge(smsa, smsa.temp, by = "SMSA")
+  smsa.temp <- merge(smsa.pop, smsa.hhi, by = "smsa")
+  colnames(smsa) <- c("smsa", "scsa", "SMSAname")
+  smsa <- merge(smsa, smsa.temp, by = "smsa")
   
   #smsa[smsa$SMSAhhi == 1, ]
   #plot(log(smsa$SMSApop), smsa$SMSAhhi, pch = 20)
   
   # Write table
   db <- conma()
-  dbWriteTable(db, name="A_CBSA_2010", value=cbsa, overwrite=TRUE)
+  dbWriteTable(db, name="A_SMSA_1980", value=smsa, overwrite=TRUE)
   dbDisconnect(db)
 }
